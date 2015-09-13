70e510232a45817e71e73270ef447c76a2665a54
Udit Gupta <uditgupta.mail@gmail.com>
2015-09-13 18:10:47 +0530

    Adding parse push notification.

--- app/src/main/java/uditgupta/tsplocation/TSPLocationApplication.java
+++ app/src/main/java/uditgupta/tsplocation/TSPLocationApplication.java
@@ -2,6 +2,9 @@ package uditgupta.tsplocation;
 
 import android.app.Application;
 
+import com.parse.Parse;
+import com.parse.ParseInstallation;
+
 /**
  * Created by udigupta on 9/13/15.
  */
@@ -10,5 +13,7 @@ public class TSPLocationApplication extends Application {
 	@Override
 	public void onCreate() {
 		super.onCreate();
+		Parse.initialize(this, "IF7m1lvlDcDRueKjYUg8RnfyTNAKVh35tJKyKKHp", "uzKrUwoWXNJ6Otb5mdQIlxXc5jmed1cdSB8R15ZX");
+		ParseInstallation.getCurrentInstallation().saveInBackground();
 	}
 }
