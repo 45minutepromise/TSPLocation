346a117db2f3a5b42de0551bfac44d1aba94bd6f
Udit Gupta <uditgupta.mail@gmail.com>
2015-09-13 17:08:41 +0530

    Adding Parse

---
+++ app/src/main/java/uditgupta/tsplocation/TSPLocationApplication.java
@@ -0,0 +1,14 @@
+package uditgupta.tsplocation;
+
+import android.app.Application;
+
+/**
+ * Created by udigupta on 9/13/15.
+ */
+public class TSPLocationApplication extends Application {
+
+	@Override
+	public void onCreate() {
+		super.onCreate();
+	}
+}
